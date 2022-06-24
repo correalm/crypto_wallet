20e07d33a439bd07427adeb4eecfe03f89d6e51c
Miguel <luismigueldasilva.99@gmail.com>
2022-06-24 17:52:57 -0300

    translate on realtime

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    # session funciona da mesma forma -> session[:nameOfSession] = "value" 
+    cookies[:course] = "Ruby on Rails"
     @name = params[:name]
   end
 end
